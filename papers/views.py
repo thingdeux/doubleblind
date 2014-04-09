80118bddbfd7179ed2d72f6d75bbd305ba803b69
Joshua Johnson <me@josh.land>
2014-04-09 16:45:57 -0700

    Finished - v1 of the index page.

--- papers/views.py
+++ papers/views.py
@@ -5,9 +5,12 @@ from django.utils import timezone
 
 from papers.models import Sender, Paper, Question, Answer
 
+WEBSITE_NAME = "Doubleblind"
+
 # Create your views here.
 def IndexView(request):
 	template_name = 'papers/index.html'
 	latest_senders = Sender.objects.all()
 
-	return render(request, 'papers/index.html', {'latest_senders': latest_senders} )
+	return render(request, 'papers/index.html', {'latest_senders': latest_senders,
+												'site_name': WEBSITE_NAME,} )
