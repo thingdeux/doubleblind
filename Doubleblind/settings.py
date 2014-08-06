2e490a225025c90bd2595403daa28c05f9fe5601
Joshua Johnson <me@josh.land>
2014-08-06 01:20:37 -0700

    Adding example, fixed errors in css while taking screenshots.

--- Doubleblind/settings.py
+++ Doubleblind/settings.py
@@ -79,5 +79,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 STATIC_URL = '/static/'
